da7f2dd337971d4be5cac496bf6c18c1f1f5d027
Ulises Vargas <texanico@gmail.com>
2016-09-16 16:26:33 -0400

    Operaciones CRUD desde sequelize

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,47 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Crear empleados
+router.post('/employees', function(req, res) {
+  models.Employee.create({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    mobile: req.body.mobile
+  }).then(function(employee) {
+    res.json(employee);
+  });
+});
+
+//Buscar todos los empleados
+router.get('/getEmployees', function(req, res){
+  models.Employee.findAll({}).then(function(getEmployees) {
+    res.json(getEmployees);
+  });
+});
+
+//Buscar un solo empleado
+router.get('/employee/:id', function(req, res) {
+  models.Employee.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(idEmployee) {
+    res.json(idEmployee);
+  });
+});
+
+//Borrar un solo empleado
+router.delete('/employee/:id', function(req, res) {
+  models.Employee.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(deleteEmployee) {
+    res.json(deleteEmployee);
+  });
+});
+
 //Comprobar conexión a DB
 models.sequelize.authenticate().then(function(err) {
     console.log('La conexión a Base de Datos se ha establecido correctamente');
